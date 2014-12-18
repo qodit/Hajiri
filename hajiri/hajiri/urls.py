01174ba1158848972c2310ee362f5e7cc6a4d58e
Anup pokhrel <virtualanup@gmail.com>
2014-12-18 14:57:30 +0545

    Few changes

--- hajiri/hajiri/urls.py
+++ hajiri/hajiri/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'pages.views.home', name='home'),
     url(r'^graph$', 'pages.views.graph', name='graph'),
-
+    url(r'^(?P<teacher_id>\d+)$', 'pages.views.teacherinfo', name='teacherinfo'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- hajiri/pages/views.py
+++ hajiri/pages/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.http import HttpResponse,HttpRequest,HttpResponseRedirect
-from pages.models import Record
+from pages.models import Record, Teacher
+from django.shortcuts import render,redirect,get_object_or_404
+
+def teachernames():
+    teachernames = []
+    teachers = Teacher.objects.all()
+    for teacher in teachers:
+        teachernames.append((teacher.pk, teacher.name))
+    return teachernames
 
 def home(request):
     import datetime
@@ -19,17 +27,40 @@ def home(request):
         'title': "Hajiri Dashboard",
         'mainmenuindex': 1,
         'teacherrecords': teachers,
+        'teachers': teachernames(),
     })
 
     return HttpResponse(template.render(context))
 
 def graph(request):
     records = Record.objects.all()
+    teachers = Teacher.objects.all()
+
+    teachername = []
+    for teacher in teachers:
+        teachername.append(teachers)
+
+    data = []
+    for record in records:
+        onerecord = []
 
     template = loader.get_template('graph.html')
     context = RequestContext(request, {
         'title': "Hajiri Graph for Teachers",
         'mainmenuindex': 1,
+        'teachers': teachernames(),
+    })
+
+    return HttpResponse(template.render(context))
+
+def teacherinfo(request, teacher_id):
+    teacher = get_object_or_404(Teacher, pk=teacher_id)
+    template = loader.get_template('teacher.html')
+    context = RequestContext(request, {
+        'title': "Info about ",
+        'mainmenuindex': 1,
+        'teachers': teachernames(),
+        'teacher': teacher,
     })
 
     return HttpResponse(template.render(context))
