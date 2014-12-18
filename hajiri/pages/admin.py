01286d5a0f0f99c67f45504fc59c59524541e111
Anup pokhrel <virtualanup@gmail.com>
2014-12-18 11:58:19 +0545

    Added some models

--- hajiri/pages/admin.py
+++ hajiri/pages/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from pages.models import Period, Subject, Teacher
 
-# Register your models here.
+
+class PeriodAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Period, PeriodAdmin)
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Subject, SubjectAdmin)
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Teacher, TeacherAdmin)
--- hajiri/pages/models.py
+++ hajiri/pages/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from hajiri import settings
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Period(models.Model):
+    teacher = models.ForeignKey(Teacher)
+    subject = models.ForeignKey(Subject)
+
+    def __str__(self):
+        return self.subject.name+" by "+self.teacher.name
+
